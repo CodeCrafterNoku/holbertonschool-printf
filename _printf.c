bc3a02f0e409596c0230af6e52ce564108202285
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 17:17:15 +0000

    happy coding

--- _printf.c
+++ _printf.c
@@ -11,7 +11,6 @@
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
-
 	return (write(1, &c, 1));
 }
 
@@ -38,11 +37,37 @@ int print_string(va_list args)
  * print_percent - Prints a percent sign
  * @args: List of arguments (unused)
  * Return: Number of characters printed (1)
- */
+*/
 int print_percent(va_list args)
 {
 	(void)args;
 	return (write(1, "%", 1));
+}
+
+/**
+ * get_print_func - Finds the appropriate function to handle a specifier
+ * @spec: The format specifier character
+ * Return: A pointer to the handling function, or NULL if not found
+ */
+int (*get_print_func(char spec))(va_list)
+{
+	format_t formats[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'\0', NULL}
+	};
+	int j = 0;
+
+	while (formats[j].spec != '\0')
+	{
+		if (spec == formats[j].spec)
+		{
+			return (formats[j].func);
+		}
+		j++;
+	}
+	return (NULL);
 }
 
 /**
@@ -53,49 +78,37 @@ int print_percent(va_list args)
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, count = 0, j;
+	int i = 0, count = 0;
 	int (*func)(va_list);
-
-	format_t formats[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'\0', NULL}
-	};
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
-
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			i++;
-			if (format[i] == '\0')
+			i++; /* Move past '%' */
+			if (format[i] == '\0') /* Handle trailing '%' */
 				return (-1);
 
-			func = NULL;
-			for (j = 0; formats[j].spec != '\0'; j++)
-			{
-				if (format[i] == formats[j].spec)
-				{
-					func = formats[j].func;
-					break;
-				}
-			}
-
+			func = get_print_func(format[i]);
 			if (func != NULL)
+			{
 				count += func(args);
+			}
 			else
 			{
+				/* Handle unknown specifier by printing '%' and the character */
 				count += write(1, &format[i - 1], 1);
 				count += write(1, &format[i], 1);
 			}
 		}
 		else
+		{
 			count += write(1, &format[i], 1);
+		}
 		i++;
 	}
 
